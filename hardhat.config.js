082b5a27b3ce43b18b163c46546c587ab6cfb46d
Prasoon <prasoon1308@gmail.com>
2023-04-02 00:46:14 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+      gasLimit: 5000000,
+    },
+  },
+};
